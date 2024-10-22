62cac0aa3c6df36507f93d9adaf20d4ed69d191c
wht-riv <2310594h@stu.kobe-u.ac.jp>
2024-10-22 14:01:47 +0900

    initial commit! 1

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 # これはサンプルの Python スクリプトです。
 
 # Shift+F10 を押して実行するか、ご自身のコードに置き換えてください。
-# Shift を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
-
+# Shift を2回押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
 
 def print_hi(name):
     # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
     print(f'Hi, {name}')  # Ctrl+F8を押すとブレークポイントを切り替えます。
 
-
 # ガター内の緑色のボタンを押すとスクリプトを実行します。
 if __name__ == '__main__':
-    a="mayu"
-    print_hi(a)
+    my_name = input("your name? ")  # インデント修正
+    print_hi(my_name)
 
 
 # PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
